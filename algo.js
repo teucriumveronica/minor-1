207ce2dccf7f47978b66697edeee9ff184069143
teucriumveronica <150184524+teucriumveronica@users.noreply.github.com>
2023-11-07 23:12:41 +0300

    Add files via upload

---
+++ algo.js
@@ -0,0 +1,58 @@
+function verify() {
+    console.log("a")
+    let a = parseFloat(elementA.value);
+    console.log(a)
+
+    if (a == 0) {
+        result = "Ноль"
+        document.getElementById("result").innerText = messageText + result;
+        document.getElementsByName('result')[0].value = result;
+        check = true;
+    } else if (a>0) {
+        result = "Положительное"
+        document.getElementById("result").innerText = messageText + result;
+        document.getElementsByName('result')[0].value = result;
+        check = true;
+    } else if (a<0) {
+        result = "Отрицательное"
+        document.getElementById("result").innerText = messageText + result;
+        document.getElementsByName('result')[0].value = result;
+        check = true;
+    } else{
+        result = "ААААА"
+        document.getElementById("result").innerText = messageText + result;
+        document.getElementsByName('result')[0].value = result;
+        check = false;
+    } 
+}
+
+function send() {
+    if (check) {
+        let textCondition = document.getElementsByTagName('p')[0].innerText
+        document.getElementsByName('formulation')[0].value = textCondition;
+        document.getElementsByName('result')[0].value = result;
+        document.getElementById("UserEnter").submit();
+    } else {
+        alert("Есть недостатки. Повторите ввод")
+    }
+}
+
+function verify_send() {
+    verify();
+    send();
+}
+
+let messageText = document.getElementById("result").innerText
+console.log(messageText)
+let result;
+let check = false;
+
+const elementA = document.getElementById("a");
+elementA.addEventListener('input', verify);
+
+const elementVerify = document.getElementById("verify");
+elementVerify.addEventListener('click', verify);
+
+const elementSend = document.getElementById("send");
+elementSend.addEventListener('click', send)
+
