16db3485cb9cd2be45362eaebfa9fe515cbbd135
teucriumveronica <150184524+teucriumveronica@users.noreply.github.com>
2023-11-07 22:34:33 +0300

    Add files via upload

---
+++ pasp.js
@@ -0,0 +1,14 @@
+const myElement = document.getElementById('sur');
+myElement.addEventListener('click', function() {
+    myElement.textContent = 'Gromyko';
+});
+
+const myElement2 = document.getElementById('name');
+myElement2.addEventListener('click', function() {
+    myElement2.textContent = 'Ekaterina';
+});
+
+const myElement3 = document.getElementById('otch');
+myElement3.addEventListener('click', function() {
+    myElement3.textContent = 'Sergeevna';
+});
